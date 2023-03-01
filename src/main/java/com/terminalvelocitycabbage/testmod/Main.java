b1805ad0dcaad14c4237ba02903fb99f3a4ab130
Brandon Davis <neusfear@gmail.com>
2023-03-01 14:48:29 -0600

    Create testmod

--- src/main/java/com/terminalvelocitycabbage/testmod/Main.java
+++
@@ -1,5 +0,0 @@
-package com.terminalvelocitycabbage.testmod;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/com/terminalvelocitycabbage/testmod/TestMod.java
@@ -0,0 +1,22 @@
+package com.terminalvelocitycabbage.testmod;
+
+import com.terminalvelocitycabbage.engine.Entrypoint;
+
+public class TestMod extends Entrypoint {
+
+    public static final String ID = "testmod";
+
+    public TestMod() {
+        super(ID);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
