9201c1defe5c3314787aa8a7925fd21c1c77888d
Brandon Davis <neusfear@gmail.com>
2023-05-16 14:45:57 -0500

    Update game to use VFS

--- src/client/java/com/terminalvelocitycabbage/testmod/client/TestModClientEntrypoint.java
+++ src/client/java/com/terminalvelocitycabbage/testmod/client/TestModClientEntrypoint.java
@@ -4,7 +4,11 @@ import com.terminalvelocitycabbage.engine.Entrypoint;
 import com.terminalvelocitycabbage.engine.client.ClientBase;
 import com.terminalvelocitycabbage.engine.debug.Log;
 import com.terminalvelocitycabbage.engine.event.HandleEvent;
+import com.terminalvelocitycabbage.engine.filesystem.ResourceSource;
+import com.terminalvelocitycabbage.engine.filesystem.ResourceType;
+import com.terminalvelocitycabbage.engine.filesystem.sources.ModSource;
 import com.terminalvelocitycabbage.engine.mod.ModClientEntrypoint;
+import com.terminalvelocitycabbage.engine.registry.Identifier;
 import com.terminalvelocitycabbage.templates.events.ServerLifecycleEvent;
 
 @ModClientEntrypoint()
@@ -20,6 +24,24 @@ public class TestModClientEntrypoint extends Entrypoint {
     @Override
     public void init() {
         Log.info("Mod init");
+
+        //Register and init filesystem things
+        //Create resource sources for this client
+        ResourceSource testModSource = new ModSource(ID, ClientBase.getInstance().getModManager().getMod(this));
+        Identifier sourceIdentifier = identifierOf("testMod_jar_resource_source");
+        //Define roots for these resources
+        testModSource.registerDefaultSourceRoot(ResourceType.MODEL);
+        testModSource.registerDefaultSourceRoot(ResourceType.TEXTURE);
+        testModSource.registerDefaultSourceRoot(ResourceType.ANIMATION);
+        testModSource.registerDefaultSourceRoot(ResourceType.SHADER);
+        testModSource.registerDefaultSourceRoot(ResourceType.SOUND);
+        testModSource.registerDefaultSourceRoot(ResourceType.DEFAULT_CONFIG);
+        //register this source to the filesystem
+        ClientBase.getInstance().getFileSystem().registerResourceSource(sourceIdentifier, testModSource);
+
+        //Register Resources
+        //TODO add jar resources
+        ClientBase.getInstance().getFileSystem().registerResource(sourceIdentifier, ResourceType.DEFAULT_CONFIG, "testmod.toml");
     }
 
     @Override
