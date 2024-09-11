e29500f0853565d6386dc81447546d9afdf7f175
Brandon Davis <neusfear@gmail.com>
2024-09-10 19:15:25 -0500

    Add localization tests

--- src/client/java/com/terminalvelocitycabbage/testmod/client/TestModClientEntrypoint.java
+++ src/client/java/com/terminalvelocitycabbage/testmod/client/TestModClientEntrypoint.java
@@ -9,12 +9,19 @@ import com.terminalvelocitycabbage.engine.filesystem.sources.ModSource;
 import com.terminalvelocitycabbage.engine.mod.ModClientEntrypoint;
 import com.terminalvelocitycabbage.engine.mod.ModEntrypoint;
 import com.terminalvelocitycabbage.engine.registry.Identifier;
+import com.terminalvelocitycabbage.engine.translation.Language;
+import com.terminalvelocitycabbage.engine.translation.Localizer;
+import com.terminalvelocitycabbage.game.client.GameClient;
+import com.terminalvelocitycabbage.game.client.registry.GameLocalizedTexts;
+import com.terminalvelocitycabbage.game.common.events.ModLocalizedTextRegistryEvent;
 import com.terminalvelocitycabbage.templates.events.ServerLifecycleEvent;
 
 import static com.terminalvelocitycabbage.testmod.common.TestMod.ID;
 
 @ModClientEntrypoint()
 public class TestModClientEntrypoint extends ModEntrypoint {
+
+    Identifier modLocalizedText;
 
     public TestModClientEntrypoint() {
         super(ID);
@@ -26,6 +33,7 @@ public class TestModClientEntrypoint extends ModEntrypoint {
 
         //Register Event Listeners
         ClientBase.getInstance().getEventDispatcher().listenToEvent(ServerLifecycleEvent.STARTED, (event) -> onServerInit((ServerLifecycleEvent) event));
+        ClientBase.getInstance().getEventDispatcher().listenToEvent(ModLocalizedTextRegistryEvent.EVENT, (event) -> registerLocalizedTexts(((ModLocalizedTextRegistryEvent) event).getLocalizer()));
 
         //Register and init filesystem things
         //Create resource sources for this client
@@ -38,11 +46,17 @@ public class TestModClientEntrypoint extends ModEntrypoint {
         testModSource.registerDefaultSourceRoot(ResourceType.SHADER);
         testModSource.registerDefaultSourceRoot(ResourceType.SOUND);
         testModSource.registerDefaultSourceRoot(ResourceType.DEFAULT_CONFIG);
+        testModSource.registerDefaultSourceRoot(ResourceType.LOCALIZATION);
         //register this source to the filesystem
         ClientBase.getInstance().getFileSystem().registerResourceSource(sourceIdentifier, testModSource);
 
         //Register Resources
         ClientBase.getInstance().getFileSystem().registerResource(sourceIdentifier, ResourceType.DEFAULT_CONFIG, "testmod.toml");
+        ClientBase.getInstance().getFileSystem().registerResource(sourceIdentifier, ResourceType.LOCALIZATION, "en-us.toml");
+    }
+
+    private void registerLocalizedTexts(Localizer localizer) {
+        modLocalizedText = localizer.registerTranslatableText(ID, "modstuff.modtest");
     }
 
     @Override
@@ -51,6 +65,18 @@ public class TestModClientEntrypoint extends ModEntrypoint {
 
         //Test
         //testFileSystemRegistryStuff();
+        var localizer = ((GameClient) ClientBase.getInstance()).getLocalizer();
+        Log.info(localizer.localize(GameLocalizedTexts.HELLO));
+        Log.info(localizer.localize(GameLocalizedTexts.GOODBYE));
+        Log.info(localizer.localize(GameLocalizedTexts.ANOTHER_TRANSLATION));
+        Log.info(localizer.localize(GameLocalizedTexts.TEST, "one", "two"));
+        Log.info(localizer.localize(modLocalizedText));
+        localizer.changeLanguage(Language.SPANISH_SPAIN);
+        Log.info(localizer.localize(GameLocalizedTexts.HELLO));
+        Log.info(localizer.localize(GameLocalizedTexts.GOODBYE));
+        Log.info(localizer.localize(GameLocalizedTexts.ANOTHER_TRANSLATION));
+        Log.info(localizer.localize(GameLocalizedTexts.TEST, "uno", "dos"));
+        Log.info(localizer.localize(modLocalizedText));
     }
 
     //Test that this mod has access to resources from both itself and the client
